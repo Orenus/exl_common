26d8ce6b49753943d789472a56670d18c63514fe
Oren Sea <oren@comversion.com>
2018-12-12 20:34:47 +0200

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['exl_helpers'],
     package_dir={'exl_helpers': '.'},
     description='Base package for helper functions and classes for exl_base_apps',
-    version='0.0.4',
+    version='0.0.5',
     url='http://github.com/prodops/package_example',
     author='Oren Sea',
     author_email='oren@prodops.io',
