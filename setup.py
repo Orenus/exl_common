02898b0007d15ad82828bcfaea4cd5ae89cb8cd9
Oren Sea <oren@comversion.com>
2018-12-24 17:57:25 +0200

    preventing publish to pypi

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     packages=['exl_helpers'],
     package_dir={'exl_helpers': '.'},
     description='Base package for helper functions and classes for exl_base_apps',
+    classifiers=[
+        'Private :: Do Not Upload to pypi server',
+    ],
     version='0.0.12',
     url='http://github.com/prodops/package_example',
     author='Oren Sea',
